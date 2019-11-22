a403256c9324016ce37e7a63a91993d7129f3e95
kim-ly <SoSe2018>
2019-11-22 17:08:30 +0100

    Adapted add series screen layout and removed showing stored data in add screen

--- app/src/main/java/com/example/seriesbacklog/ui/AddActivity.kt
+++ app/src/main/java/com/example/seriesbacklog/ui/AddActivity.kt
@@ -2,14 +2,9 @@ package com.example.seriesbacklog.ui
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProviders
 import com.example.seriesbacklog.R
-import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_add.*
-import kotlinx.android.synthetic.main.content_add.*
-import java.util.*
 
 class AddActivity : AppCompatActivity() {
 
@@ -21,58 +16,17 @@ class AddActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         supportActionBar?.title = "Add Series"
-
-        initViews()
-        initViewModel()
-    }
-
-    private fun initViews() {
-        fab.setOnClickListener {
-            editViewModel.series.value?.apply {
-                title = seriesTitle.text.toString()
-                platform = seriesPlatform.text.toString()
-                day = seriesDay.text.toString()
-                month = seriesMonth.text.toString()
-                year = seriesYear.text.toString()
-
-            }
-            editViewModel.updateSeries()
-        }
-    }
-
-    private fun initViewModel() {
-        editViewModel = ViewModelProviders.of(this).get(EditViewModel::class.java)
-        editViewModel.series.value = intent.extras?.getParcelable(EXTRA_NOTE)!!
-
-        editViewModel.series.observe(this, Observer { series ->
-            if (series != null) {
-                seriesTitle.setText(series.title)
-                seriesPlatform.setText(series.platform)
-                seriesDay.setText(series.day)
-                seriesMonth.setText(series.month)
-                seriesYear.setText(series.year)
-            }
-        })
-
-        editViewModel.error.observe(this, Observer { message ->
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-        })
-
-        editViewModel.success.observe(this, Observer { success ->
-            if (success) finish()
-        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item?.itemId) {
-            android.R.id.home -> { // Used to identify when the user has clicked the back button
+            android.R.id.home -> {
                 finish()
                 true
             }
-            else -> super.onOptionsItemSelected(item)
+            else -> return super.onOptionsItemSelected(item)
         }
     }
-
 
     companion object {
         const val EXTRA_NOTE = "EXTRA_NOTE"
