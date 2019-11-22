2e3ea583402617fbb13487652b36367197e7eb08
kim-ly <SoSe2018>
2019-11-22 17:15:41 +0100

    Implemented saving series data by clicking on save button in add series screen

--- app/src/main/java/com/example/seriesbacklog/ui/AddActivity.kt
+++ app/src/main/java/com/example/seriesbacklog/ui/AddActivity.kt
@@ -1,10 +1,17 @@
 package com.example.seriesbacklog.ui
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.seriesbacklog.R
+import com.example.seriesbacklog.model.Series
 import kotlinx.android.synthetic.main.activity_add.*
+import kotlinx.android.synthetic.main.content_add.*
+
+const val EXTRA_SERIES = "EXTRA_SERIES"
 
 class AddActivity : AppCompatActivity() {
 
@@ -14,8 +21,26 @@ class AddActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
         setSupportActionBar(toolbar)
-
         supportActionBar?.title = "Add Series"
+        fab.setOnClickListener { onSaveClick() }
+    }
+
+    private fun onSaveClick() {
+        if (seriesTitle.text.toString().isNotBlank() &&
+            seriesPlatform.text.toString().isNotBlank() &&
+            seriesDay.text.toString().isNotBlank() &&
+            seriesMonth.text.toString().isNotBlank() &&
+            seriesYear.text.toString().isNotBlank()) {
+
+            val series = Series(seriesTitle.text.toString(), seriesPlatform.text.toString(), seriesDay.text.toString(), seriesMonth.text.toString(), seriesYear.text.toString() )
+            val resultIntent = Intent()
+            resultIntent.putExtra(EXTRA_SERIES, series)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        } else {
+            Toast.makeText(this,"Fill in all the fields"
+                , Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
